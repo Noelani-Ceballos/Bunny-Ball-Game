d669bdd712f4e19807a8f197bba4c81763403fdf
Noelani-Ceballos <noelanic3@nycstudents.net>
2024-05-13 09:21:14 -0400

    The game has instructions = telling the player what to do

---
+++ Assets/Scripts/Instructions.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instructions : MonoBehaviour
+{
+    private string message = "Move using W, A, S, D to control the character. Go through the maze and collect keys";
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(message);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
